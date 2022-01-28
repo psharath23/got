3c8db212a9845166e146cc540517baaedea6687a
bablu.sharath23@gmail.com <psharath1995@gmail.com>
2022-01-28 19:49:21 +0530

    adde dsome extra stuff

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -6,11 +6,15 @@ const url = "https://hp-assessment-api.herokuapp.com/got/"
 
 const HouseList = () => {
     const [text, setText] = useState("")
-    const [characters, setHouses] = useState([])
+    const [characters, setCharacters] = useState([])
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         fetch(url + "characters")
             .then(res => res.json())
-            .then(res => setHouses(res.rows))
+            .then(res => {
+                setCharacters(res.rows)
+                setLoading(false)
+            })
     }, [])
 
     const filterHouses = (h) => text.length ? h.name.toLowerCase().includes(text.toLowerCase()) : true
@@ -19,10 +23,10 @@ const HouseList = () => {
         setText(e.target.value)
     }
 
-    return <div>
+    return <div className="character-list-container">
         <h2>CHARACTERS</h2>
         <input type="text" placeholder="start typing a character name" onChange={handleTextChange} />
-        <ul className="character-list">
+        {!loading && characters.length > 0 && <ul className="character-list">
             {
                 characters.filter(filterHouses).map((c) => {
                     return <li className="character">
@@ -33,7 +37,13 @@ const HouseList = () => {
                     </li>
                 })
             }
-        </ul>
+        </ul>}
+        {
+            !loading && characters.length == 0 && <div>No Characters found</div>
+        }
+        {
+            loading && <div>Loading Characters, please wait ... </div>
+        }
     </div>
 }
 
